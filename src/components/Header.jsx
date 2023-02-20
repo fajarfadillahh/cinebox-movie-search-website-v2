6a040e2e14d47e91f9950e40d989d080a668733f
fajarfadillahh <fajaragustian979@gmail.com>
2023-02-20 19:06:19 +0700

    modified: add sticky Header components when user scrolling down

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { RiMenu3Fill, RiCloseFill } from "react-icons/ri";
 
@@ -8,9 +8,22 @@ import NavbarMobile from "./Navbar/NavbarMobile";
 
 const Header = () => {
   const [navbarOpen, setNavbarOpen] = useState(false);
+  const [stickyHeader, setStickyHeader] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      window.scrollY > 5 ? setStickyHeader(true) : setStickyHeader(false);
+    });
+  }, []);
 
   return (
-    <header className="fixed top-0 left-0 right-0 z-50 mx-auto max-w-[1440px]">
+    <header
+      className={`fixed top-0 left-0 right-0 z-50 mx-auto max-w-[1440px] transition ${
+        stickyHeader
+          ? "bg-gray-900/80 backdrop-blur-lg"
+          : "bg-transparent backdrop-blur-none"
+      }`}
+    >
       <div className="container flex h-24 items-center justify-between md:relative">
         <Link to="/" className="section-title text-[24px]">
           Cinebox<span className="text-blue-600">.</span>
