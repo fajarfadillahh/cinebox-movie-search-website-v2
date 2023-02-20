46662fc3895e19f0a3560e0ff9992b36049e7cec
fajarfadillahh <fajaragustian979@gmail.com>
2023-02-20 15:56:50 +0700

    fixed: responsive Hero section - Home page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <section className="section relative pt-40 xl:pt-48">
       <div className="container">
-        <div className="grid justify-items-center gap-8">
+        <div className="grid gap-8">
           <div className="text-center">
             <h1 className="section-title pb-3 text-[42px] lg:text-[56px]">
               Find your favorite movies <span className="text-blue-600">.</span>
@@ -14,11 +14,11 @@ const Hero = () => {
             </p>
           </div>
 
-          <form className="flex flex-col gap-2 sm:flex-row sm:items-center">
+          <form className="flex flex-col gap-2 sm:flex-row sm:items-center sm:justify-self-center">
             <input
               type="text"
               placeholder="Search a movie, tv show, or more..."
-              className="flex h-[52px] w-full bg-gray-800 px-4 text-center font-title font-semibold text-white outline-none placeholder:font-sans placeholder:font-medium placeholder:-tracking-tighter placeholder:text-gray-400 sm:w-[450px] sm:text-left"
+              className="flex h-[52px] w-full bg-gray-800 px-4 text-center font-title font-semibold text-white outline-none placeholder:font-sans placeholder:font-medium placeholder:-tracking-tighter placeholder:text-gray-400 sm:w-[350px] sm:text-left lg:w-[450px]"
             />
             <button
               type="submit"
