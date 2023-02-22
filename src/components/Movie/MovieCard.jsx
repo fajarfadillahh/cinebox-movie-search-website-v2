260f2ee715c1eaee23638f18d65a5cbbd5bec222
fajarfadillahh <fajaragustian979@gmail.com>
2023-02-22 15:32:05 +0700

    modified: changed format date at the Data file & MovieCard

--- src/components/Movie/MovieCard.jsx
+++ src/components/Movie/MovieCard.jsx
@@ -4,6 +4,14 @@ import React from "react";
 import TMDBIcon from "../../assets/images/tmdb-icon.svg";
 
 const MovieCard = ({ poster, releaseDate, title, rating }) => {
+  // change format date
+  const formatDate = (dateString) => {
+    const options = { month: "short", day: "numeric", year: "numeric" };
+    const date = new Date(dateString);
+
+    return date.toLocaleDateString("en-US", options);
+  };
+
   return (
     <div className="group grid gap-6">
       <div className="overflow-hidden">
@@ -15,7 +23,9 @@ const MovieCard = ({ poster, releaseDate, title, rating }) => {
       </div>
 
       <div className="overflow-hidden p-1">
-        <p className="section-text pb-3 text-[14px]">{releaseDate}</p>
+        <p className="section-text pb-3 text-[14px]">
+          {formatDate(releaseDate)}
+        </p>
         <h3 className="section-title truncate pb-2 text-[22px]">{title}</h3>
         <div className="flex items-center gap-3">
           <img src={TMDBIcon} alt="tmdb icon" className="w-7" />
